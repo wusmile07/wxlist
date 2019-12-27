d32f867a627c13a214c512bf7006465f74f73c6d
wu <1224>
2019-12-27 12:14:03 +0800

    二维码

--- create/index/index.js
+++ create/index/index.js
@@ -1,6 +1,13 @@
-
+import drawQrcode from '../../utils/weapp.qrcode.esm.js'
 Page({
-
+    sh(){
+        drawQrcode({
+            width: 200,
+            height: 200,
+            canvasId: 'myQrcode',
+            text: 'https://github.com/yingye',
+        })
+    }
 })
 
 
